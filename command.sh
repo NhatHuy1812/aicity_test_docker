e0079bd593db8601ccafb1e7141821628ed13a86
Pham Dinh Trung Hieu <phamdinhtrunghieu1103@gmail.com>
2025-07-18 21:04:50 +0700

    ok'

--- command.sh
+++ command.sh
@@ -7,7 +7,7 @@
 #   ${IMAGE} \
 #   --confidence_threshold 0.50
 
-IMAGE="hieupham1103/aicity_dfine:dual"
+IMAGE="hieupham1103/aicity_dfine:latest"
 DATA_DIR="/home/becamex/FishEye8K/challenge_iccv_2025_hieu/data"
 
 docker run -it --ipc=host --runtime=nvidia -v ${DATA_DIR}:/data ${IMAGE}
